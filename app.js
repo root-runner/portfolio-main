142f21dc36ed0d97aa46079b1b453c59bf46119a
Ryan Haste <rmhaste@gmail.com>
2022-11-24 15:11:34 +1100

    mobile menu
    animated mobile menu to transition on click of hamburger.

--- app.js
+++ app.js
@@ -6,7 +6,7 @@ mobileBtn.addEventListener("click", () => {
    nav.classList.add("menu-btn");
 });
 
-mobileBtnExit.addEventListener("click", () => {
+nav.addEventListener("click", () => {
    nav.classList.remove("menu-btn");
 });
 
